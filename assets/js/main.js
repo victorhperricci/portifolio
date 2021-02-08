c790cd41e651adbc7c4eeccc85588ac6d315bd46
Victor Hugo <valvesperricci@yahoo.com.br>
2021-02-07 22:48:30 -0300

    slide adicionado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,26 @@
-import { c, cs, initCloneProject } from './modules/clone-projects.js'
-import initType from './modules/maquina-de-escrever.js'
-import initScrollAnimate from './modules/scroll-animate.js'
-import initProjectsScroll from './modules/scroll-projectParImpar.js'
-import initShowBackTop from './modules/button-back-top.js'
-import initScrollSuave from './modules/scroll-suave.js'
-import {initShowMenu, initWhenScrollRemoveMenu} from './modules/menu-mobile.js'
-import initChangeHeader from './modules/change-header.js'
+import { c, cs, initCloneProject } from "./modules/clone-projects.js";
+import initType from "./modules/maquina-de-escrever.js";
+import initScrollAnimate from "./modules/scroll-animate.js";
+import initProjectsScroll from "./modules/scroll-projectParImpar.js";
+import initShowBackTop from "./modules/button-back-top.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import {
+  initShowMenu,
+  initWhenScrollRemoveMenu,
+} from "./modules/menu-mobile.js";
+import initChangeHeader from "./modules/change-header.js";
+import SlideNav from "./modules/slide.js";
 
 initCloneProject(c, cs);
 initType(c, cs);
 initScrollAnimate(c, cs);
 initProjectsScroll(c, cs);
 initShowBackTop(c, cs);
-initScrollSuave(c, cs)
+initScrollSuave(c, cs);
 initShowMenu(c, cs);
-initWhenScrollRemoveMenu(c, cs)
-initChangeHeader(c, cs)
+initWhenScrollRemoveMenu(c, cs);
+initChangeHeader(c, cs);
 
-
-
-
-
-
-
-
-
-
-
-
+const slide = new SlideNav(".content-project", ".content-project-area");
+slide.init();
+slide.addControl();
---
+++ assets/js/modules/slide.js
@@ -0,0 +1,214 @@
+import debounce from "./debounce.js";
+
+class Slide {
+  constructor(wrapper, slide) {
+    this.wrapper = document.querySelector(wrapper);
+    this.slide = document.querySelector(slide);
+
+    this.dist = { finalPostion: 0, startX: 0, movement: 0 };
+    this.activeClass = "active";
+    this.changeEvent = new Event("changeEvent");
+  }
+
+  transition(active) {
+    this.slide.style.transition = active ? ".3s" : "";
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  updatePostion(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPostion - this.dist.movement;
+  }
+
+  onStart(event) {
+    let movetype;
+    if (event.type === "mousedown") {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      movetype = "mousemove";
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      movetype = "touchmove";
+    }
+
+    this.wrapper.addEventListener(movetype, this.onMove);
+    this.transition(false);
+  }
+
+  onMove(event) {
+    const pointerPosition =
+      event.type === "mousemove"
+        ? event.clientX
+        : event.changedTouches[0].clientX;
+    const finalPositon = this.updatePostion(pointerPosition);
+    this.moveSlide(finalPositon);
+  }
+
+  onEnd(event) {
+    const moveType = event.type === "mouseup" ? "mousemove" : "touchmove";
+    this.wrapper.removeEventListener(moveType, this.onMove);
+    this.dist.finalPostion = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < 120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("touchstart", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+    this.wrapper.addEventListener("touchend", this.onEnd);
+  }
+
+  // Slides Config
+
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return { element, position };
+    });
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index];
+    this.moveSlide(this.slideArray[index].position);
+    this.slideIndexNav(index);
+    this.dist.finalPostion = activeSlide.position;
+    this.changeActiveClass();
+    this.wrapper.dispatchEvent(this.changeEvent);
+  }
+
+  slideIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    };
+  }
+
+  activePrevSlide() {
+    if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) this.changeSlide(this.index.next);
+  }
+
+  changeActiveClass() {
+    this.slideArray.forEach((item) =>
+      item.element.classList.remove(this.activeClass)
+    );
+    this.slideArray[this.index.active].element.classList.add(this.activeClass);
+  }
+
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000);
+  }
+
+  addResizeEvent() {
+    window.addEventListener("resize", this.onResize);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+
+    this.onResize = debounce(this.onResize.bind(this), 200);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    this.slidesConfig();
+    this.transition(true);
+    this.addResizeEvent();
+    this.changeSlide(0);
+
+    return this;
+  }
+}
+
+export default class SlideNav extends Slide {
+  constructor(wrapper, slide) {
+    super(wrapper, slide);
+    this.addBindControlEvents();
+  }
+
+  addArrow(prev, next) {
+    this.prevElement = document.querySelector(prev);
+    this.nextElement = document.querySelector(next);
+    this.addArrowEvent();
+  }
+
+  addArrowEvent() {
+    this.prevElement.addEventListener("click", this.activePrevSlide);
+    this.nextElement.addEventListener("click", this.activeNextSlide);
+  }
+
+  createControls() {
+    const control = document.createElement("ul");
+    control.dataset.control = "slide";
+
+    this.slideArray.forEach((item, index) => {
+      control.innerHTML += `<li>
+        <a href="#slide${index + 1}">${index + 1}</a>
+      </li>`;
+    });
+    this.wrapper.appendChild(control);
+    return control;
+  }
+
+  eventControls(item, index) {
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+      this.changeSlide(index);
+    });
+    this.wrapper.addEventListener("changeEvent", this.activeControlItem);
+  }
+
+  activeControlItem() {
+    this.controlArray.forEach((item) =>
+      item.classList.remove(this.activeClass)
+    );
+    this.controlArray[this.index.active].classList.add(this.activeClass);
+  }
+
+  addControl(customControl) {
+    this.control =
+      document.querySelector(customControl) || this.createControls();
+    this.controlArray = [...this.control.children];
+
+    this.activeControlItem();
+    this.controlArray.forEach(this.eventControls);
+  }
+
+  addBindControlEvents() {
+    this.eventControls = this.eventControls.bind(this);
+    this.activeControlItem = this.activeControlItem.bind(this);
+  }
+}
